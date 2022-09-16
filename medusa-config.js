daa37ab99771901db315689b76af220f83066fc5
Abhishek Rawat <hi@rawat.dev>
2022-09-16 12:32:56 +0400

    adding  procfile and updating start script

--- medusa-config.js
+++ medusa-config.js
@@ -26,7 +26,7 @@ try {
 const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8080";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
@@ -68,10 +68,10 @@ module.exports = {
   projectConfig: {
     // redis_url: REDIS_URL,
     // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    // database_database: "./medusa-db.sql",
+    // database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
     database_extra:
